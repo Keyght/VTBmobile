8a58e41e9c52dbdf9f6cf3a7152ee4f16ad2700d
Kubirill <Kubirill@rambler.ru>
2021-10-09 01:01:39 +0300

    Add open submenu

---
+++ Assets/Scripts/Investing/MainButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainButtons : MonoBehaviour
+{
+    public Animator anim;
+    public Animator subMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // anim.GetComponentInParent<Animator>();
+        subMenu.SetBool("Hide", true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        anim.SetBool("Hide", true);
+        subMenu.gameObject.SetActive(true);
+        subMenu.SetBool("Hide", false);
+    }
+}
