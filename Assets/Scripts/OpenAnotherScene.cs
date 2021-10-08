a260fba62dfdd50509516d8e3b858dc2ca140fa3
Marina <mbakanova2016@yandex.ru>
2021-10-08 22:33:26 +0300

    check ui

---
+++ Assets/Scripts/OpenAnotherScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenAnotherScene : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void OnMouseDown()
+    {
+        LoadScene();
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+   
+}
