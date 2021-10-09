3b48153a7c70097d460b2ae14a44be7aca5901fa
Kubirill <Kubirill@rambler.ru>
2021-10-09 12:39:11 +0300

    Beginband

---
+++ Assets/Scripts/Investing/BondsControll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BondsControll : MonoBehaviour
+{
+
+
+    float percent;
+    public float minPercent;
+    public float maxPercent;
+    public Text percentField;
+    public Text stonksField;
+
+    public int period;
+    public int lengthBonds;
+    public int nominal;
+    public int risk;
+
+    
+    
+    // 1/100000
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
